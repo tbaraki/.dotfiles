fd0bee418d891fea7adc2d297c7dd13d60e7c1b5
Travis Baraki <travis@tbaraki.net>
2024-01-23 16:26:23 -0800

    add nvim

---
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "terraformls", "taplo", "yamlls", "bashls", "cssls", "html", "jsonls", "tsserver", "marksman"}
+             })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+
+            lspconfig.lua_ls.setup({})
+            lspconfig.tsserver.setup({})
+            lspconfig.terraformls.setup({})
+            lspconfig.cssls.setup({})
+
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+        end
+    }
+}
