b77b23f738777731d10f8a85f0cf10e3d1a70515
Travis Baraki <travis@tbaraki.net>
2024-01-24 17:46:08 -0800

    add lint/formatting, alpha splash screen to nvim

---
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,25 @@
+return {
+    "nvimtools/none-ls.nvim",
+    config = function()
+        local null_ls = require("null-ls")
+
+        null_ls.setup({
+            sources = {
+                -- lua
+                null_ls.builtins.formatting.stylua,
+                -- JS/etc
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.diagnostics.eslint_d,
+                -- Terraform
+                null_ls.builtins.diagnostics.terraform_validate,
+                null_ls.builtins.diagnostics.tfsec,
+                null_ls.builtins.formatting.terraform_fmt,
+                -- Python
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+            },
+        })
+
+        vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+}
