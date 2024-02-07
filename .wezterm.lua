6773013ec06322b75874fcce15c11441d94c54d5
Travis Baraki <travis@tbaraki.net>
2024-02-06 20:56:47 -0800

    add wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,9 @@
+
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+--config.color_scheme = 'Catppuccin Macchiato (Gogh)'
+config.color_scheme = 'catppuccin-mocha'
+
+return config
