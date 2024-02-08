6ad4925a6c213b20f92dd36401b046d8d301f7f5
Travis Baraki <travis@tbaraki.net>
2024-02-07 17:59:09 -0800

    Starship and wezterm tweak/fmt

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,8 @@
-
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
 --config.color_scheme = 'Catppuccin Macchiato (Gogh)'
-config.color_scheme = 'catppuccin-mocha'
+config.color_scheme = "catppuccin-mocha"
 
 return config
