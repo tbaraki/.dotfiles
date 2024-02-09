d4a805c321ed9c2f4a633570b1ac7403bfddcdc9
Travis Baraki <travis@tbaraki.net>
2024-02-08 19:44:29 -0800

    nvim config tweak, rust_analyzer

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -20,6 +20,8 @@ return {
 
 			lspconfig.lua_ls.setup({})
 			lspconfig.terraformls.setup({})
+            lspconfig.rust_analyzer.setup({})
+
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
