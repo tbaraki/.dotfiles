449858c3ccc902c2bc98c32ad49631148d70bff7
Travis Baraki <travis@tbaraki.net>
2024-04-29 08:00:41 -0700

    minor git updates

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,31 +1,44 @@
 return {
-	"goolord/alpha-nvim",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  "goolord/alpha-nvim",
+  cmd = "Alpha",
+  opts = function()
+    local dashboard = require "alpha.themes.dashboard"
+    dashboard.section.header.val = {
+      " █████  ███████ ████████ ██████   ██████",
+      "██   ██ ██         ██    ██   ██ ██    ██",
+      "███████ ███████    ██    ██████  ██    ██",
+      "██   ██      ██    ██    ██   ██ ██    ██",
+      "██   ██ ███████    ██    ██   ██  ██████",
+      " ",
+      "    ███    ██ ██    ██ ██ ███    ███",
+      "    ████   ██ ██    ██ ██ ████  ████",
+      "    ██ ██  ██ ██    ██ ██ ██ ████ ██",
+      "    ██  ██ ██  ██  ██  ██ ██  ██  ██",
+      "    ██   ████   ████   ██ ██      ██",
+    }
+    dashboard.section.header.opts.hl = "DashboardHeader"
+    dashboard.section.footer.opts.hl = "DashboardFooter"
 
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.startify")
+    local button, get_icon = require("astronvim.utils").alpha_button, require("astronvim.utils").get_icon
+    dashboard.section.buttons.val = {
+      button("LDR n  ", get_icon("FileNew", 2, true) .. "New File  "),
+      button("LDR f f", get_icon("Search", 2, true) .. "Find File  "),
+      button("LDR f o", get_icon("DefaultFile", 2, true) .. "Recents  "),
+      button("LDR f w", get_icon("WordFile", 2, true) .. "Find Word  "),
+      button("LDR f '", get_icon("Bookmarks", 2, true) .. "Bookmarks  "),
+      button("LDR S l", get_icon("Refresh", 2, true) .. "Last Session  "),
+    }
 
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
-
-		alpha.setup(dashboard.opts)
-	end,
+    dashboard.config.layout = {
+      { type = "padding", val = vim.fn.max { 2, vim.fn.floor(vim.fn.winheight(0) * 0.2) } },
+      dashboard.section.header,
+      { type = "padding", val = 5 },
+      dashboard.section.buttons,
+      { type = "padding", val = 3 },
+      dashboard.section.footer,
+    }
+    dashboard.config.opts.noautocmd = true
+    return dashboard
+  end,
+  config = require "plugins.configs.alpha",
 }
--- .wezterm.lua
+++ .wezterm.lua
@@ -2,7 +2,39 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
---config.color_scheme = 'Catppuccin Macchiato (Gogh)'
 config.color_scheme = "catppuccin-mocha"
+config.hide_tab_bar_if_only_one_tab = true
+config.initial_rows = 48
+config.initial_cols = 128
+config.use_fancy_tab_bar = false
+config.enable_wayland = false --Set to 'false' if Wezterm fails to load in Wayland.
+
+config.colors = {
+	tab_bar = {
+		background = "#181825", --Catppuccin Mantle
+		active_tab = {
+			bg_color = "#313244", --Catppuccin Surface0
+			fg_color = "#94e2d5", --Catppuccin Teal
+			intensity = "Bold",
+			underline = "Single",
+		},
+		inactive_tab = {
+			bg_color = "#1e1e2e", --Catppuccin Base
+			fg_color = "#cdd6f4", --Catppuccin Text
+		},
+		inactive_tab_hover = {
+			bg_color = "#45475a", --Catppuccin Surface1
+			fg_color = "#cdd6f4", --Catppuccin Text
+		},
+		new_tab = {
+			bg_color = "#1e1e2e", --Catppuccin Base
+			fg_color = "#cdd6f4", --Catppuccin Text
+		},
+		new_tab_hover = {
+			bg_color = "#45475a", --Catppuccin Surface1
+			fg_color = "#cdd6f4", --Catppuccin Text
+		},
+	},
+}
 
 return config
